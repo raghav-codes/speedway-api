b0f93937cd294a835cfdc9a53870628d8b62633b
varadharajanthiagarajan <varadha.001@gmail.com>
2021-04-20 18:53:43 -0400

    green commit -  implemented the controller post method

---
+++ src/main/java/com/example/speedwayapi/DriverController.java
@@ -0,0 +1,18 @@
+package com.example.speedwayapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DriverController {
+
+    @PostMapping("driver")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addDriver(){
+
+    }
+}
