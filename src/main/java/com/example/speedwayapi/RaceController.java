e132a784191034cb801794c4ef186d5cffd0202c
maanick <maanickavasagam@gmail.com>
2021-04-21 16:48:34 -0600

    Green Test for Post raceevent

---
+++ src/main/java/com/example/speedwayapi/RaceController.java
@@ -0,0 +1,19 @@
+package com.example.speedwayapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class RaceController {
+
+    @PostMapping("raceevent")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addRaceEvent(){
+
+    }
+
+
+}
